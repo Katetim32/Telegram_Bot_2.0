d2ff926983ffb66e05048855e123d222d1acc9dd
Katetim32 <ekaterinatimireva1508@gmail.com>
2023-09-29 17:59:22 +0300

    Initial Commit

--- bot.py
+++ bot.py
@@ -4,13 +4,14 @@ from aiogram import Bot, Dispatcher, types, F, html
 from aiogram.enums.dice_emoji import DiceEmoji
 from aiogram.filters.command import Command, CommandObject
 from config_reader import config
+from datetime import datetime
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 #Объект бота
 # Для записей с типом Secret* необходимо
 # вызывать метод get_secret_value(),
 # чтобы получить настоящее содержимое вместо '*******'
-bot = Bot(token=config.bot_token.get_secret_value())
+bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 #Диспетчер
 dp = Dispatcher()
 
@@ -53,6 +54,40 @@ async def cmd_name(message: types.Message, command: CommandObject): #тк исп
         await message.answer(f"Привет, {html.bold(html.quote(command.args))}!", parse_mode="HTML") #методы bold и quote для экранирования и форматирования
     else:
         await message.answer("Укажите, пожалуйста, свое имя после команды /name.")
+
+#Сохранение форматирования
+"""@dp.message(F.text)
+async def echo_with_time(message: types.Message):
+    # Получаем текущее время в часовом поясе ПК
+    time_now = datetime.now().strftime('%H:%M')
+    #Создаем подчеркнутый текст создания сообщения
+    added_text = html.underline(f"Создано в {time_now}")
+    # Отправляем полученное ботом сообщение с созданным временем
+    await message.answer(f"{message.html_text}\n {added_text}", parse_mode="HTML")
+"""
+
+@dp.message(F.text)
+async def extract_data(message: types.Message):
+    data = {
+        "url": "<N/A>",
+        "email": "<N/A>",
+        "code": "<N/A>"
+    }
+    entities = message.entities or []
+    for item in entities:
+        if item.type in data.keys():
+            # Неправильно
+            # data[item.type] = message.text[item.offset : item.offset+item.length]
+            # Правильно
+            data[item.type] = item.extract_from(message.text)
+
+    await message.reply(
+        "Вот что я нашёл:\n"
+        f"URL: {html.quote(data['url'])}\n"
+        f"E-mail: {html.quote(data['email'])}\n"
+        f"Пароль: {html.quote(data['code'])}"
+    )
+
 #Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot, mylist = [1, 2, 3])
